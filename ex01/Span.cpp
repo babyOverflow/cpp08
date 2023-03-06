673eab42ff4d5a4c66da22a74e6f7f0977719992
seonghyk <seonghyk@student.42seoul.kr>
2023-03-06 12:55:46 +0900

    impl span functions

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,6 +1,8 @@
-#include "Span.h"
 #include <exception>
 #include <stdexcept>
+#include <algorithm>
+#include <limits>
+#include "Span.h"
 
 Span::Span()
 	: mNumbers()
@@ -31,4 +33,30 @@ void Span::addNumber(int number)
 	if (mNumbers.size() == mNumbers.capacity())
 		throw std::out_of_range("Span");
 	mNumbers.push_back(number);
+}
+
+int Span::shortestSpan() const
+{
+	if (mNumbers.size() < 2)
+		throw std::out_of_range("Span");
+	std::vector<int> tmp(mNumbers);
+	std::sort(tmp.begin(), tmp.end());
+	
+	int diff = std::numeric_limits<int>::max();
+	iterator it = tmp.begin();
+	while (it != tmp.end())
+	{
+		int a = *it;
+		int b = *(++it);
+		if (a - b < diff)
+			diff = a - b;
+	}
+	return diff;
+}
+
+int Span::longestSpan() const
+{
+	std::vector<const int>::iterator itmax = std::max_element(mNumbers.begin(), mNumbers.end());
+	std::vector<const int>::iterator itmin = std::max_element(mNumbers.begin(), mNumbers.end());
+	return (*itmax - *itmin);
 }
--- ex01/Span.h
+++ ex01/Span.h
@@ -8,7 +8,12 @@ public:
 	Span& operator=(const Span& other);
 	~Span();
 
+	typedef std::vector<int>::iterator iterator;
+	typedef std::vector<int>::iterator const_iterator;
+
 	void addNumber(int number);
+	int shortestSpan() const;
+	int longestSpan() const ;
 
 private:
 	std::vector<int> mNumbers;
